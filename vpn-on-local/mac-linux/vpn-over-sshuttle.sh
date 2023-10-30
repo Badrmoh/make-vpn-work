1b73d02dc21ccbdc24e49166b74ad1db86e25920
Badr Ibrahim <engbadr@outlook.com>
2023-10-30 12:19:40 +0100

    Update vpn-over-sshuttle.sh

--- vpn-on-local/mac-linux/vpn-over-sshuttle.sh
+++ vpn-on-local/mac-linux/vpn-over-sshuttle.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(realpath $(dirname $0))
+VPN_SERVER_IP=${VPN_SERVER_IP:?Provide VPN Server IP address!}
 cd $SCRIPT_PATH
 
 if [[ $(uname -s) == "Darwin" ]];then
@@ -9,7 +10,7 @@ fi
 
 tunnel_up(){
   if [[ ! -f relay-host-sshuttle.pid ]];then
-    sshuttle -D --pidfile relay-host-sshuttle.pid -r relay-host 3.66.235.60:1194
+    sshuttle -D --pidfile relay-host-sshuttle.pid -r relay-host ${VPN_SERVER_IP:?Provide VPN Server IP address!}
   fi
 }
 
